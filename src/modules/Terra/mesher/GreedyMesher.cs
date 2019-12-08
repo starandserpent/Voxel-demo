cae2a0e55957eab3dc61d7a2a5f5f2fc8f28978a
Pawlost <mr.p.balusek@gmail.com>
2019-12-08 10:11:47 +0100

    Corrected faces

--- src/modules/Terra/mesher/GreedyMesher.cs
+++ src/modules/Terra/mesher/GreedyMesher.cs
@@ -55,15 +55,13 @@ public class GreedyMesher
 
             if (vertices[objectID - 1] == null)
             {   
-                    Vector3[] buffer =  memory.Rent(chunk.voxels.Length * 6);
-                    vertices[objectID - 1] = buffer;
+                Vector3[] buffer =  memory.Rent(chunk.voxels.Length * 6);
+                vertices[objectID - 1] = buffer;
             }
 
             int z = count / 4096;
             int y = count % 64;
             int x = (count - 4096 * z) / 64;
-
-            int origin = x + (z * 64);
             
             float sx = x* 0.25f;
             float sy = y* 0.25f;
@@ -75,313 +73,231 @@ public class GreedyMesher
 
             //Front
             Vector3[] vectors = vertices[objectID - 1];
-            /*            if (z > 0)
-            {
-                int pos = (x + (z - 1) * 64);
-                if (prevBox.deleteIndex[0] > -1)
-                {
-                    prevBox = boxes[texture].Span[prevBox.deleteIndex[0]];
-                }
 
-                if (box.vertice[0][3].y <= prevBox.vertice[0][3].y)
-                {
-                    box.deleteIndex[0] = x + (z - 1) * 64;
-                }
+            int index = indice[objectID - 1];
+            //Front
+            //1
+            vectors[index].x = sx;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
 
-                if (prevBox.vertice[0][3].y < box.vertice[0][3].y && prevBox.vertice[0][0].y >= box.vertice[0][0].y)
-                {
-                    box.vertice[0][0] =
-                        new TerraVector3(box.vertice[0][0].x, prevBox.vertice[0][3].y, box.vertice[0][0].z);
-                    box.vertice[0][1] =
-                        new TerraVector3(box.vertice[0][1].x, prevBox.vertice[0][3].y, box.vertice[0][1].z);
-                    box.vertice[0][5] =
-                        new TerraVector3(box.vertice[0][5].x, prevBox.vertice[0][3].y, box.vertice[0][5].z);
-                }
-            }*/
-                        int index = indice[objectID - 1];
+            //2
+            vectors[index].x = ax;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
+
+            //3
+             vectors[index].x = ax;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
+
+            //4
+             vectors[index].x = ax;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
+
+            //5
+           vectors[index].x = sx;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
+
+            //6
+           vectors[index].x = sx;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
+
             //Back
+            //1
             vectors[index].x = ax;
             vectors[index].y = sy;
             vectors[index].z = az;
             index++;
 
+            //2
             vectors[index].x = sx;
             vectors[index].y = sy;
             vectors[index].z = az;
             index++;
 
+            //3
              vectors[index].x = sx;
             vectors[index].y = ay;
             vectors[index].z = az;
             index++;
 
+            //4
              vectors[index].x = sx;
             vectors[index].y = ay;
             vectors[index].z = az;
             index++;
 
+            //5
            vectors[index].x = ax;
             vectors[index].y = ay;
             vectors[index].z = az;
             index++;
 
+            //6
            vectors[index].x = ax;
             vectors[index].y = sy;
             vectors[index].z = az;
             index++;
-
-            indice[objectID - 1] = index;
-
-             index = indice[objectID - 1];
-            //Back
-            vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-            vectors[index].x = sx;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-            indice[objectID - 1] = index;
-
-
-             index = indice[objectID - 1];
-            //Back
-            vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-            vectors[index].x = sx;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-            indice[objectID - 1] = index;
-
-             index = indice[objectID - 1];
-            //Back
-            vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-            vectors[index].x = sx;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-            indice[objectID - 1] = index;
-
-             index = indice[objectID - 1];
-            //Back
-            vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-            vectors[index].x = sx;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-                        indice[objectID - 1] = index;
-
-
-             index = indice[objectID - 1];
-            //Back
-            vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-            vectors[index].x = sx;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-             vectors[index].x = sx;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = ay;
-            vectors[index].z = az;
-            index++;
-
-           vectors[index].x = ax;
-            vectors[index].y = sy;
-            vectors[index].z = az;
-            index++;
-
-            indice[objectID - 1] = index;
-        /*
-            //Right
-            box.deleteIndex[2] = -1;
-            box.vertice[2] = new TerraVector3[6]
-            {
-                new TerraVector3(x, y, z + 1) / 4, new TerraVector3(x, y, z) / 4,
-                new TerraVector3(x, y + lenght, z) / 4, new TerraVector3(x, y + lenght, z) / 4,
-                new TerraVector3(x, y + lenght, z + 1) / 4, new TerraVector3(x, y, z + 1) / 4
-            };
-            if (x > 0)
-            {
-                int pos = (x - 1) + z * 64;
-                Box prevBox = boxes[texture].Span[pos];
-                if (prevBox.deleteIndex[2] > -1)
-                {
-                    prevBox = boxes[texture].Span[prevBox.deleteIndex[2]];
-                }
-
-                if (box.vertice[2][3].y <= prevBox.vertice[2][3].y)
-                {
-                    box.deleteIndex[2] = pos;
-                }
-
-                if (prevBox.vertice[2][3].y < box.vertice[2][3].y && prevBox.vertice[2][0].y >= box.vertice[2][0].y)
-                {
-                    box.vertice[2][0] =
-                        new TerraVector3(box.vertice[2][0].x, prevBox.vertice[2][3].y, box.vertice[2][0].z);
-                    box.vertice[2][1] =
-                        new TerraVector3(box.vertice[2][1].x, prevBox.vertice[2][3].y, box.vertice[2][1].z);
-                    box.vertice[2][5] =
-                        new TerraVector3(box.vertice[2][5].x, prevBox.vertice[2][3].y, box.vertice[2][5].z);
-                }
-            }
-
 
             //Left
-            box.deleteIndex[3] = -1;
-            box.vertice[3] = new TerraVector3[6]
-            {
-                new TerraVector3(x + 1, y, z) / 4, new TerraVector3(x + 1, y, z + 1) / 4,
-                new TerraVector3(x + 1, y + lenght, z + 1) / 4, new TerraVector3(x + 1, y + lenght, z + 1) / 4,
-                new TerraVector3(x + 1, y + lenght, z) / 4, new TerraVector3(x + 1, y, z) / 4
-            };
+            //1
+            vectors[index].x = sx;
+            vectors[index].y = sy;
+            vectors[index].z = az;
+            index++;
 
-            if (x > 0)
-            {
-                int pos = (x - 1) + z * 64;
-                Box prevBox = boxes[texture].Span[pos];
-                if (prevBox.vertice[3][3].y > box.vertice[3][3].y && prevBox.vertice[3][0].y <= box.vertice[3][0].y)
-                {
-                    prevBox.vertice[3][0] = new TerraVector3(prevBox.vertice[3][0].x, box.vertice[3][3].y,
-                        prevBox.vertice[3][0].z);
-                    prevBox.vertice[3][1] = new TerraVector3(prevBox.vertice[3][1].x, box.vertice[3][3].y,
-                        prevBox.vertice[3][1].z);
-                    prevBox.vertice[3][5] = new TerraVector3(prevBox.vertice[3][5].x, box.vertice[3][3].y,
-                        prevBox.vertice[3][5].z);
-                    boxes[texture].Span[pos] = prevBox;
-                }
-                else if (prevBox.vertice[3][3].y <= box.vertice[3][3].y)
-                {
-                    prevBox.deleteIndex[3] = origin;
-                    boxes[texture].Span[pos] = prevBox;
-                }
-            }
+            //2
+            vectors[index].x = sx;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
+
+            //3
+             vectors[index].x = sx;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
+
+            //4
+            vectors[index].x = sx;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
+
+            //5
+           vectors[index].x = sx;
+            vectors[index].y = ay;
+            vectors[index].z = az;
+            index++;
+
+            //6
+            vectors[index].x = sx;
+            vectors[index].y = sy;
+            vectors[index].z = az;
+            index++;
+
+            //Right
+           //1
+            vectors[index].x = ax;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
+
+            //2
+            vectors[index].x = ax;
+            vectors[index].y = sy;
+            vectors[index].z = az;
+            index++;
+
+            //3
+             vectors[index].x = ax;
+            vectors[index].y = ay;
+            vectors[index].z = az;
+            index++;
+
+            //4
+            vectors[index].x = ax;
+            vectors[index].y = ay;
+            vectors[index].z = az;
+            index++;
+
+            //5
+           vectors[index].x = ax;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
+
+            //6
+            vectors[index].x = ax;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
 
             //Top
-            box.deleteIndex[4] = -1;
-            box.vertice[4] = new TerraVector3[6]
-            {
-                new TerraVector3(x, y + lenght, z) / 4, new TerraVector3(x + 1, y + lenght, z) / 4,
-                new TerraVector3(x + 1, y + lenght, z + 1) / 4, new TerraVector3(x + 1, y + lenght, z + 1) / 4,
-                new TerraVector3(x, y + lenght, z + 1) / 4, new TerraVector3(x, y + lenght, z) / 4
-            };
+            //1
+            vectors[index].x = sx;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
 
-            //Bottom        
-            box.deleteIndex[5] = -1;
-            box.vertice[5] = new TerraVector3[6]
-            {
-                new TerraVector3(x + 1, y, z) / 4, new TerraVector3(x, y, z) / 4, new TerraVector3(x, y, z + 1) / 4,
-                new TerraVector3(x, y, z + 1) / 4, new TerraVector3(x + 1, y, z + 1) / 4,
-                new TerraVector3(x + 1, y, z) / 4
-            };^&*/
+            //2
+            vectors[index].x = ax;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
+
+            //3
+             vectors[index].x = ax;
+            vectors[index].y = ay;
+            vectors[index].z = az;
+            index++;
+                
+            //4 
+             vectors[index].x = ax;
+            vectors[index].y = ay;
+            vectors[index].z = az;
+            index++;
+
+            //5
+           vectors[index].x = sx;
+            vectors[index].y = ay;
+            vectors[index].z = az;
+            index++;
+
+            //6
+            vectors[index].x = sx;
+            vectors[index].y = ay;
+            vectors[index].z = sz;
+            index++;
+
+            //Bottom
+            //1
+            vectors[index].x = ax;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
+
+            //2
+            vectors[index].x = sx;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
+
+            //3
+             vectors[index].x = sx;
+            vectors[index].y = sy;
+            vectors[index].z = az;
+            index++;
+
+            //4
+            vectors[index].x = sx;
+            vectors[index].y = sy;
+            vectors[index].z = az;
+            index++;
+
+            //5
+           vectors[index].x = ax;
+            vectors[index].y = sy;
+            vectors[index].z = az;
+            index++;
+
+        //6
+           vectors[index].x = ax;
+            vectors[index].y = sy;
+            vectors[index].z = sz;
+            index++;
+
+            indice[objectID - 1] = index;
             count += lenght;
         }
         
@@ -398,34 +314,33 @@ public class GreedyMesher
 
             Texture texture = registry.SelectByID(t + 1).texture;
 
-            Vector3[] vertice = new Vector3[indice[t]];
-            Vector3[] normals = new Vector3[indice[t]];
-            Vector2[] uvs = new Vector2[indice[t]];
+            int size = indice[t];
+            Vector3[] vertice = new Vector3[size];
+            Vector3[] normals = new Vector3[size];
+            Vector2[] uvs = new Vector2[size];
 
             float textureWidth = 2048f / texture.GetWidth();
             float textureHeight = 2048f / texture.GetHeight();
 
-                Vector3[] primitives = vertices[t];
-            for (int i = 0; i < indice[t]; i ++)    
+            Vector3[] primitives = vertices[t];
+            for (int i = 0; i < size; i ++)    
             { 
-                int s = i%6;
                 Vector3 vector = primitives[i];
-
                 if(vector != null){
+                int s = ((i%36))/6;
                 vertice[i] = vector;
-
-                switch(s){
+                switch(s) {
                     case 0:
                     normals[i].x = 0f;
                 normals[i].y = 0f;
-                normals[i].z = 1f;
+                normals[i].z = -1f;
                     uvs[i].x = vector.x * textureWidth;
                     uvs[i].y = vector.y * textureHeight;
                     break;
                     case 1:
                     normals[i].x = 0f;
                 normals[i].y = 0f;
-                normals[i].z = -1f;
+                normals[i].z = 1f;
                     uvs[i].x = vector.x * textureWidth;
                     uvs[i].y = vector.y * textureHeight;
                     break;
