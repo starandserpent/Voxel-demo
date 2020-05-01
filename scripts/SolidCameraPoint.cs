8eb68a8b91c2c6c7b68a4fbba9dd21067e59f7f8
Pavel Balusek <mr.p.balusek@gmail.com>
2020-05-02 00:19:16 +0200

    Switched scene

--- scripts/SolidCameraPoint.cs
+++ scripts/SolidCameraPoint.cs
@@ -1,22 +1,22 @@
 using Godot;
 public class SolidCameraPoint : LoadMarker
 {
-    private GameController gameController;
-    private Camera camera;
-    public override void _Ready()
-    {
-        VisualServer.SetDebugGenerateWireframes(true);
-        gameController = (GameController) FindParent("SceneController");
-        camera = (Camera) FindNode("Camera");
-        gameController.Prepare(camera, GetWorld().Scenario);
-        Input.SetMouseMode(Input.MouseMode.Captured);
-    }
+	private GameController gameController;
+	private Camera camera;
+	public override void _Ready()
+	{
+		VisualServer.SetDebugGenerateWireframes(true);
+		gameController = (GameController) FindParent("SceneController");
+		camera = (Camera) FindNode("Camera");
+		gameController.Prepare(camera, GetWorld().Scenario);
+		Input.SetMouseMode(Input.MouseMode.Captured);
+	}
 
-    public override void _PhysicsProcess(float delta)
-    {
-    }
-    public override void _Process(float delta)
-    {
-        gameController.Generate(this);
-    }
+	public override void _PhysicsProcess(float delta)
+	{
+	}
+	public override void _Process(float delta)
+	{
+		gameController.Generate(this);
+	}
 }
